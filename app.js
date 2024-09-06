a889724c461dd5ca1cc5b314174344b229d96bf7
João Rezende <70391407+rezendejp@users.noreply.github.com>
2024-09-06 11:16:24 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function pesquisar(){
+console.log("clicou");
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+let resultados = ""
+
+for (let dado of dados) {
+    resultados += `
+    <div class="item-resultado">
+        <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informações</a>
+    </div>`}
+
+section.innerHTML = resultados}
+
+
+
---
+++ dados.js
@@ -0,0 +1,11 @@
+let dados = [
+    {titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista profissional brasileira. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a medalha de bronze nos Jogos Olímpicos de Tóquio 2020. Sua habilidade e carisma a tornaram um ícone do skate mundial.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"},
+    {titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua vitória histórica marcou o judô brasileiro e a tornou um dos maiores nomes do esporte nacional. Com sua técnica impecável e determinação, Beatriz é um exemplo para as futuras gerações de judocas.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"},
+    {titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com suas incríveis habilidades e determinação. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com um currículo repleto de conquistas e feitos históricos. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo do esporte nacional. Com sua alegria contagiante e carisma, Rebeca Andrade é um exemplo para jovens atletas e um orgulho para todo o país.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"}];
+
